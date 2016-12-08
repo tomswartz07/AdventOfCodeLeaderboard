4779adf39397e8abbf24fbbd50a3ee836eeb728f
Tom Swartz <tom.swartz07@gmail.com>
2016-12-07 19:30:01 -0500

    Tweak formatting
    Fix a few issues with the formatting and remove the private leaderboard
    ID number. Get your own, ya filthy animal.

--- scripts/leaderboard.py
+++ scripts/leaderboard.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
 
-import requests
-
 '''
 This script will grab the leaderboard from Advent of Code and post it to Slack
 '''
 
-LEADERBOARD = 'http://adventofcode.com/2016/leaderboard/private/view/108855.json'
+import requests
+
+LEADERBOARD = 'http://adventofcode.com/2016/leaderboard/private/view/<LEADERBOARD_ID>.json'
 COOKIES = {'session': 'SESSION_COOKIE'}
-# Set the webhook_url to the one provided by Slack when you create the webhook at https://my.slack.com/services/new/incoming-webhook/
+# Set the webhook_url to the one provided by Slack when you create
+# the webhook at https://my.slack.com/services/new/incoming-webhook/
 SLACK_WEBHOOK = 'https://hooks.slack.com/services/HOOK'
 
 def parseData():
@@ -19,14 +20,13 @@ def parseData():
         outputString = "Advent of Code Leaderboard as of today:\n"
         outputString += "<http://adventofcode.com/2016/leaderboard/private/view/108855|View Online Leaderboard>\n"
 
-        # get all members
+        # Get all members
         users = [(m["name"], m["stars"]) for m in members.values()]
-        # sort members by stars decending
+        # Sort members by stars decending
         users.sort(key=lambda s: -s[1])
-        # add each user to outputString
+        # Add each user to outputString
         for username, stars in users:
             outputString += "{}: {} stars\n".format(username, stars)
-
         return outputString
 
 def generatePayload(outputString):
@@ -40,9 +40,9 @@ def generatePayload(outputString):
 def postData(PAYLOAD):
     print(PAYLOAD)
     requests.post(
-            SLACK_WEBHOOK, data=PAYLOAD,
-            headers={'Content-Type': 'application/json'}
-            )
+        SLACK_WEBHOOK, data=PAYLOAD,
+        headers={'Content-Type': 'application/json'}
+        )
 
 parseData()
 generatePayload(outputString)
