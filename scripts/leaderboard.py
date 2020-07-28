c655e75afbc2ba160aa9734fe029507ff8d5135b
Tom Swartz <tom.swartz07@gmail.com>
2020-07-28 00:24:42 -0400

    PyLint fixes

--- scripts/leaderboard.py
+++ scripts/leaderboard.py
@@ -4,14 +4,24 @@
 This script will grab the leaderboard from Advent of Code and post it to Slack
 '''
 
-import datetime, json, requests
+import datetime
+import sys
+import json
+import requests
 
 # see README for directions on how to fill these variables
 LEADERBOARD_ID = ""
 SESSION_ID = ""
 SLACK_WEBHOOK = ""
 
+# You should not need to change this URL
+LEADERBOARD_URL = "https://adventofcode.com/{}/leaderboard/private/view/{}".format(
+        datetime.datetime.today().year, LEADERBOARD_ID)
+
 def formatLeaderMessage(members):
+    """
+    Format the message to conform to Slack's API
+    """
     message = ""
 
     # add each member to message
@@ -23,6 +33,9 @@ def formatLeaderMessage(members):
     return message
 
 def parseMembers(members_json):
+    """
+    Handle member lists from AoC leaderboard
+    """
     # get member name, score and stars
     members = [(m["name"], m["local_score"], m["stars"]) for m in members_json.values()]
 
@@ -32,6 +45,9 @@ def parseMembers(members_json):
     return members
 
 def postMessage(message):
+    """
+    Post the message to to Slack's API in the proper channel
+    """
     payload = json.dumps({
         "icon_emoji": ":christmas_tree:",
         "username": "Advent Of Code Leaderboard",
@@ -45,22 +61,23 @@ def postMessage(message):
     )
 
 def main():
+    """
+    Main program loop
+    """
     # make sure all variables are filled
     if LEADERBOARD_ID == "" or SESSION_ID == "" or SLACK_WEBHOOK == "":
-        print("Please update script variables before running script.\nSee README for details on how to do this.")
-        exit(1)
-
-    global LEADERBOARD_URL
-    LEADERBOARD_URL = "https://adventofcode.com/{}/leaderboard/private/view/{}".format(datetime.datetime.today().year, LEADERBOARD_ID)
+        print("Please update script variables before running script.\n\
+                See README for details on how to do this.")
+        sys.exit(1)
 
     # retrieve leaderboard
     r = requests.get(
         "{}.json".format(LEADERBOARD_URL),
         cookies={"session": SESSION_ID}
     )
-    if r.status_code != requests.codes.ok:
+    if r.status_code != requests.codes.ok: #pylint: disable=no-member
         print("Error retrieving leaderboard")
-        exit(1)
+        sys.exit(1)
 
     # get members from json
     members = parseMembers(r.json()["members"])
